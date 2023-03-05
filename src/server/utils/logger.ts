f2ca09377ec091101b29d017eda54be5efeb04dc
Keiji Miyazaki <miyah.dev@gmail.com>
2023-03-05 12:02:17 +0900

    Fix type error

--- src/server/utils/logger.ts
+++ src/server/utils/logger.ts
@@ -1,23 +1,23 @@
 export const perfLogger = {
-  async requestDidStart(requestContext) {
+  async requestDidStart() {
     console.log('[DEBUG]: Request started!');
     let prevTime: number;
     const t0 = performance.now();
 
     return {
-      async parsingDidStart(requestContext) {
+      async parsingDidStart() {
         const t1 = performance.now();
         console.log('[DEBUG]: Parsing started!', t1 - t0);
         prevTime = t1;
       },
 
-      async validationDidStart(requestContext) {
+      async validationDidStart() {
         const t2 = performance.now();
         console.log('[DEBUG]: Validation started!', t2 - prevTime);
         prevTime = t2;
       },
 
-      async willSendResponse(requestContext) {
+      async willSendResponse() {
         const t3 = performance.now();
         console.log('[DEBUG]: Sending response started! ', t3 - prevTime);
         prevTime = t3;
